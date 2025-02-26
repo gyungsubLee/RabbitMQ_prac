7e3c4969b9fdba88a1a40c3911d5311f723b9506
gyoungsubLee <dltjq2323@gmail.com>
2025-02-26 13:54:26 +0900

    project init

---
+++ src/main/java/com/prac/rabbimq/RabbimqApplication.java
@@ -0,0 +1,13 @@
+package com.prac.rabbimq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbimqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbimqApplication.class, args);
+	}
+
+}
