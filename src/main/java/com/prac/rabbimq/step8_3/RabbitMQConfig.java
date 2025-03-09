219ac001f293670b030eb873f7150fe7b8fb0e5e
gyoungsubLee <dltjq2323@gmail.com>
2025-03-10 00:08:29 +0900

    step8.3: config 네이밍 컨벤션 적용

---
+++ src/main/java/com/prac/rabbimq/step8_3/RabbitMQConfig.java
@@ -0,0 +1,48 @@
+package com.prac.rabbimq.step8_3;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String TOPIC_EXCHANGE = "order.topic.exchange";
+    public static final String TOPIC_QUEUE = "order.completed.queue";
+    public static final String TOPIC_DLQ = "order.topic.dlq";
+    public static final String TOPIC_DLX = "order.topic.dlx";
+    public static final String DEAD_LETTER_ROUTING_KEY = "dlx.#";
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(TOPIC_EXCHANGE);
+    }
+
+    @Bean
+    public TopicExchange deadletterExchange() {
+        return new TopicExchange(TOPIC_DLX);
+    }
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.durable(TOPIC_QUEUE)
+                .withArgument("x-dead-letter-exchange", TOPIC_DLX)
+                .withArgument("x-dead-letter-routing-key", DEAD_LETTER_ROUTING_KEY)
+                .build();
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return new Queue(TOPIC_DLQ);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with("order.completed.*");
+    }
+
+    @Bean
+    public Binding deadLetterBinding() {
+        return BindingBuilder.bind(deadLetterQueue()).to(deadletterExchange()).with(DEAD_LETTER_ROUTING_KEY);
+    }
+}
