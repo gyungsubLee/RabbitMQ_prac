c0ab5faca679a0f8248895b976b9204304fa185e
gyoungsubLee <dltjq2323@gmail.com>
2025-03-09 22:25:12 +0900

    step8.2: 설정 파일(application.yml)을 통한 retry 처리
    - 설정 파일(application.yml)에 Retry 설정을 통한 재처리

--- src/main/java/com/prac/rabbimq/RabbimqApplication.java
+++ src/main/java/com/prac/rabbimq/RabbimqApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.prac.rabbimq.step8_1")
+@ComponentScan(basePackages = "com.prac.rabbimq.step8_2")
 public class RabbimqApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/prac/rabbimq/step8_2/OrderController.java
@@ -0,0 +1,22 @@
+package com.prac.rabbimq.step8_2;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    private final OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> sendOrderMessage(@RequestParam String message) {
+
+        orderProducer.sendShipping(message);
+        return ResponseEntity.ok("Order Completed Message sent: " + message);
+    }
+}
